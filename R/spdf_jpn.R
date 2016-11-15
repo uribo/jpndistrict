847eb05963ce4d1a8b519e0191a6a6f3fa22ced6
Shinya Uryu <suika1127@gmail.com>
2016-11-15 09:16:25 +0900

    modified download methods

--- R/spdf_jpn.R
+++ R/spdf_jpn.R
@@ -1,67 +1,72 @@
 #' Spatial Data frame
+#'
+#' @description Prefecture polygon data.
+#' @details Download administrative area data from the National Land Numeral Information Download Service. If there is already a downloaded file,
+#' it is read by giving the path to the file as the path argument. When path is not specified, it downloads to the temporary folder.
+#' In the case of the same prefecture in the session, we will not download twice.
 #' @import dplyr
 #' @import magrittr
 #' @importFrom readr read_rds
-#' @param code jis code
+#' @param path path to ksj N03 folder
+#' @param code jis code from 1 to 47
 #' @param admin_name string
 #' @param district logical (default TRUE)
 #' @examples
-#' spdf_jpn_pref(admin_name = "岡山県", district = FALSE)
+#' \dontrun{
 #' spdf_jpn_pref(code = 33, district = FALSE)
+#' }
+#'
 #' @export
-spdf_jpn_pref <- function(code = NULL, admin_name = NULL, district = TRUE) {
+spdf_jpn_pref <- function(path, code = NULL, admin_name = NULL, district = TRUE) {
 
-  if (missing(admin_name)) {
-    pref.code <- dplyr::filter_(jpnprefs, ~jis_code == pref_code(code)) %>% magrittr::use_series(jis_code)
-  } else if (missing(code)) {
-    pref.code <- dplyr::filter_(jpnprefs, ~prefecture == admin_name) %>% magrittr::use_series(jis_code)
+  if (missing(path)) {
+    if (missing(admin_name)) {
+      pref.code <- collect_prefcode(code = code)
+    } else if (missing(code)) {
+      pref.code <- collect_prefcode(admin_name = admin_name)
+    }
+
+    d <- read_ksj_cityarea(code = as.numeric(pref.code))
+
+  } else {
+    d <- read_ksj_cityarea(path = path)
   }
 
   if (district == TRUE) {
-    d <- readr::read_rds(system.file(paste0("extdata/pref_", pref.code, "_city_spdf.rds"), package = "jpndistrict"))
+    res <- d
   } else {
-    d <- readr::read_rds(system.file(paste0("extdata/pref_", pref.code, "_spdf.rds"), package = "jpndistrict"))
+    res <- raw_bind_cityareas(d)
   }
-  return(d)
-}
-
-#' Spatial Data frame city single
-#' @import spdplyr
-#' @importFrom dplyr filter
-#' @param df data frame
-#' @param jis_code jis code
-#' @param admin_name administration name (full lenght)
-#' @examples
-#' spdf_jpn_city(spdf_jpn_pref(code = 33, district = TRUE), jis_code = 33103)
-#' spdf_jpn_city(spdf_jpn_pref(code = 33, district = TRUE), admin_name = "倉敷市")
-#' @export
-spdf_jpn_city <- function(df, jis_code, admin_name = NULL) {
-
-  if (missing(admin_name)) {
-    d <- dplyr::filter(df, city_code == jis_code)
-  } else if (missing(jis_code)) {
-    d <- dplyr::filter(df, city_name_full == admin_name)
-  }
-  return(d)
+  return(res)
 }
 
 #' Spatial Data frame cities
+#'
+#' @description City area polygon data.
 #' @import spdplyr
 #' @importFrom dplyr filter
-#' @param jis_code jis code
-#' @param admin_name string
-#' @param jis_code_pref numeric
+#' @param path path to ksj N03 folder
+#' @param jis_code_pref jis code from 1 to 47
+#' @param jis_code_city jis code for city as jis_code_pref + identifier number
+#' @param admin_name city name
 #' @examples
-#' spdf_jpn_cities(jis_code_pref = 33, jis_code = 33103)
-#' spdf_jpn_cities(jis_code_pref = 33, jis_code = c(33103, 33104, 33205))
+#' spdf_jpn_cities(jis_code_pref = 33, jis_code_city = 33103)
+#' spdf_jpn_cities(jis_code_pref = 33, jis_code_city = c(33103, 33104, 33205))
 #' @export
-spdf_jpn_cities <- function(jis_code_pref, jis_code = NULL, admin_name = NULL) {
-  d <- spdf_jpn_pref(jis_code_pref, district = TRUE)
+spdf_jpn_cities <- function(path, jis_code_pref, jis_code_city = NULL, admin_name = NULL) {
+
+  city_code <- city_name_full <- NULL
+
+  if (missing(path)) {
+      d <- spdf_jpn_pref(code = jis_code_pref, district = TRUE)
+    } else {
+      d <- spdf_jpn_pref(path = path, district = TRUE)
+    }
 
   if (missing(admin_name)) {
-    d <- dplyr::filter(d, city_code %in% jis_code)
-  } else if (missing(jis_code)) {
-    d <- dplyr::filter(d, city_name_full %in% admin_name)
+    d <- dplyr::filter(d, city_code %in% jis_code_city)
+  } else if (missing(jis_code_city)) {
+    d <- dplyr::filter(d, grepl(admin_name, city_name_full))
   }
 
   return(d)
--- R/util.R
+++ R/util.R
@@ -1,4 +1,5 @@
 #' Get prefecture code from jis code
+#'
 #' @param jis_code jis code
 pref_code <- function(jis_code) {
   res <- sprintf("%02d", as.numeric(substr(jis_code, 1, 2)))
@@ -15,6 +16,8 @@ pref_code <- function(jis_code) {
 #' @importFrom stringi stri_trim_both
 collect_ksj_p33 <- function(code = NULL, path = NULL) {
 
+  address <- NULL
+
   d <- geojsonio::geojson_read(
     paste0(path, "/", list.files(path, pattern = paste0(code, ".shp$"))),
     method = "local",
@@ -30,5 +33,137 @@ collect_ksj_p33 <- function(code = NULL, path = NULL) {
     ungroup()
 
   return(d)
+
+}
+
+#' Bind city area polygons to prefecture polygon
+#'
+#' @param path path to rds file
+#' @import dplyr
+#' @import spdplyr
+#' @importFrom geojsonio geojson_read
+#' @importFrom geojsonio geojson_write
+#' @importFrom readr read_rds
+#' @importFrom rgeos gUnaryUnion
+#' @importFrom sp SpatialPolygons
+bind_cityareas <- function(path = NULL) {
+
+  dummy <- NULL
+
+  pref.shp <- geojsonio::geojson_read(list.files(path, pattern = "shp$", full.names = TRUE),
+                                      method           = "local",
+                                      what             = "sp",
+                                      stringsAsFactors = TRUE)
+  res <- raw_bind_cityareas(pref.shp)
+
+  return(res)
+}
+
+raw_bind_cityareas <- function(pref.shp) {
+  dummy <- NULL
+  tmp.geojson <- tempfile(fileext = ".geojson")
+
+  d.merge <- rgeos::gUnaryUnion(pref.shp,
+                                id = pref.shp@data$pref_name)
+
+  geojsonio::geojson_write(sp::SpatialPolygons(d.merge@polygons[1]),
+                           lat = "lat", lon = "long",
+                           geometry = "polygon",
+                           file = tmp.geojson,
+                           verbose = FALSE)
+  res <- geojsonio::geojson_read(tmp.geojson,
+                                 method = "local",
+                                 what = "sp",
+                                 stringsAsFactors = TRUE) %>%
+    mutate(pref_name = pref.shp$pref_name[1],
+           jiscode  = as.numeric(substr(pref.shp$city_code[1], 1, 2))) %>%
+    select(-dummy)
+
+  file.remove(tmp.geojson)
+
+  return(res)
+}
+
+read_ksj_cityarea <- function(code = NULL, path = NULL) {
+
+  if (missing(path)) {
+    path <- path_ksj_cityarea(code)
+  }
+
+  res <- collect_cityarea(path)
+
+  return(res)
+
+}
+
+#' @importFrom utils download.file
+#' @importFrom utils unzip
+path_ksj_cityarea <- function(code = NULL, path = NULL) {
+
+  if (missing(path)) {
+
+    pref.identifer <- sprintf("%02d", code)
+    dest.path <- paste(tempdir(), paste0("N03-150101_", pref.identifer, "_GML.zip"), sep = "/")
+    # extract.path <- paste(tempdir(), pref.identifer,
+    #                       gsub(".zip$", "", paste0("N03-20150101_", pref.identifer, "_GML.zip")),  sep = "/")
+    extract.path <- paste(tempdir(), pref.identifer,  sep = "/")
+
+    # ksj zip file none
+    if (is.null(path) & file.exists(dest.path) == FALSE) {
+
+      utils::download.file(paste0("http://nlftp.mlit.go.jp/ksj/gml/data/N03/N03-15/N03-150101_", pref.identifer, "_GML.zip"),
+                    destfile = dest.path,
+                    method   = "wget",
+                    quiet    = TRUE)
+      utils::unzip(zipfile = dest.path,
+                   exdir   = extract.path)
+
+      path = paste(extract.path, gsub(".zip$", "", paste0("N03-20150101_", pref.identifer, "_GML.zip")), sep = "/")
+
+      # ksj zip file exist
+    } else if (file.exists(dest.path) == TRUE) {
+      path = extract.path
+    }
+
+  }
+
+  return(path)
+
+}
+
+collect_prefcode <- function(code = NULL, admin_name = NULL) {
+
+  jis_code <- NULL
+  jpnprefs <- readr::read_rds(system.file(paste0("extdata/jpnprefs.rds"), package = "jpndistrict"))
+
+  if (missing(admin_name)) {
+    pref.code <- dplyr::filter_(jpnprefs, ~jis_code == pref_code(code)) %>% magrittr::use_series(jis_code)
+  } else if (missing(code)) {
+    pref.code <- dplyr::filter_(jpnprefs, ~prefecture == admin_name) %>% magrittr::use_series(jis_code)
+  }
+
+  return(pref.code)
+}
+
+
+
+collect_cityarea <- function(path = NULL) {
+
+  N03_001 <- N03_002 <- N03_003 <- N03_004 <- N03_007 <- tmp_var <- NULL
+  pref_name <- city_name_ <- city_name <- city_name_full <- city_code <- NULL
+
+
+  geojsonio::geojson_read(list.files(path, pattern = "shp$", full.names = TRUE, recursive = TRUE),
+                          method           = "local",
+                          what             = "sp",
+                          stringsAsFactors = TRUE) %>%
+    select(-N03_002) %>%
+    mutate(N03_001 = as.character(N03_001),
+           N03_003 = as.character(N03_003),
+           N03_004 = as.character(N03_004),
+           tmp_var = ifelse(is.na(N03_003), "", N03_003),
+           city_name_full = stringi::stri_trim_both(gsub("NA", "", paste(tmp_var, N03_004)))) %>%
+    rename(pref_name = N03_001, city_name_ = N03_003, city_name = N03_004, city_code = N03_007) %>%
+    select(pref_name, city_name_, city_name, city_name_full, city_code)
 
 }
