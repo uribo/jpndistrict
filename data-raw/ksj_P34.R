13694655a4ae1addbfed817d97b6a8b4da4dc810
Shinya Uryu <suika1127@gmail.com>
2017-07-23 15:11:26 +0900

    tweak

--- data-raw/ksj_P34.R
+++ data-raw/ksj_P34.R
@@ -1,9 +1,9 @@
 library(kokudosuuchi)
-library(spdplyr)
 library(dplyr)
+library(readr)
 
-# Download shape files ------------------------------------------------------
-# 行政区域 http://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-P34.html
+# Download files ------------------------------------------------------
+# 市区町村役場データ http://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-P34.html
 df.dl.url <- kokudosuuchi::getKSJURL(identifier = "P34") %>%
   dplyr::mutate(dest_file = gsub("http://nlftp.mlit.go.jp/ksj/gml/data/P34/P34-14/", "", zipFileUrl)) %>%
   dplyr::select(identifier, year, areaCode, zipFileUrl, dest_file)
