0d44cad0c1896c23b6e503b8bc9e6f0ac3409e2d
Shinya Uryu <suika1127@gmail.com>
2017-12-05 05:17:51 +0900

    update original data KSJ N03 (2017)

--- data-raw/shp2sf.R
+++ data-raw/shp2sf.R
@@ -1,87 +1,129 @@
 # Load Employed Packages --------------------------------------------------
 # 5MB以下
-library(testthat)
 library(dplyr)
+library(purrr)
 library(magrittr)
-library(sf) # 0.5-2
+library(testthat)
+library(sf)
+# Download raw data (47 prefectures, 2017) ----------------------------------------------------------------------
+if (file.exists("data-raw/KSJ_N03/N03-170101_GML.zip") == FALSE) {
 
-# Download shape files ------------------------------------------------------
-if (file.exists("~/Dropbox/maps/shapefile/国土数値情報/N03/N03-20150101_GML/") == FALSE) {
-  library(kokudosuuchi) # 0.2.0
-  # 行政区域 http://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-N03.html
-  ids <- getKSJSummary() %>% filter(grepl("行政区域", title)) %>% use_series(identifier)
-  # 全国を選択
-  # 平成27年1月1日時点のデータ は条件を満たすことで二次利用が可能
-  dl.url <- getKSJURL(identifier = ids) %>% filter(areaCode == 0, year == 2015) %>% use_series(zipFileUrl)
+  dir.create("data-raw/KSJ_N03")
 
-  expect_equal(dl.url,
-               'http://nlftp.mlit.go.jp/ksj/gml/data/N03/N03-15/N03-150101_GML.zip')
+  d <- kokudosuuchi::getKSJURL(identifier = "N03")
+  dl_url <- d %>%
+    arrange(desc(year), areaCode) %>%
+    slice(1L) %>%
+    use_series(zipFileUrl)
 
-  download.file(dl.url,
-                destfile = '~/Dropbox/maps/shapefile/国土数値情報/N03/N03-150101_GML.zip')
-  unzip(
-    '~/Dropbox/maps/shapefile/国土数値情報/N03/N03-150101_GML.zip',
-    exdir = "~/Dropbox/maps/shapefile/国土数値情報/N03"
-  )
+  download.file(dl_url,
+                paste0("data-raw/KSJ_N03/", basename(dl_url)))
+  unzip(paste0("data-raw/KSJ_N03/", basename(dl_url)), exdir = "data-raw/KSJ_N03")
+  # add to Git Ignore
 }
 
-# 日本全体（項目の確認） -------------------------------------------------------------
-jpn_pref_shp <- read_sf('~/Dropbox/maps/shapefile/国土数値情報/N03/N03-20150101_GML/',
-                        stringsAsFactors = FALSE)
-testthat::expect_s3_class(jpn_pref_shp, c('sf', 'data.frame'))
-# 都道府県名, 支庁・振興局名(当該都道府県が「北海道」の場合、該当する支庁・振興局の名称), 郡・政令都市名, 市区町村名, 行政区域コード
-testthat::expect_named(jpn_pref_shp, c('N03_001', 'N03_002', 'N03_003', 'N03_004', 'N03_007', 'geometry'))
 
-# jpn_pref_shp %<>% #
-#   # select(-N03_002) %>%
-#   mutate(tmp_var = if_else(is.na(N03_003), "", N03_003),
-#          city_name_full = stringr::str_trim(gsub("NA", "", paste(tmp_var, N03_004)))) %>%
-#   rename(pref_name = N03_001,
-#          # sicho_shinkou_name = N03_002,
-#          city_name_ = N03_003, city_name = N03_004, city_code = N03_007) %>%
-#   select(pref_name,
-#          # sicho_shinkou_name,
-#          city_name_, city_name, city_name_full, city_code) %>%
-# 全国だから因子にするけど都道府県個別では不要
-#   mutate(pref_name = pref_name %>% factor() %>% forcats::fct_inorder()) %>%
-#   st_simplify(preserveTopology = TRUE, dTolerance = 0.020)
+# Modified shapefile ----------------------------------------------------------
+sf_japan <- st_read("data-raw/KSJ_N03/N03-17_170101.shp") %>%
+  set_names(c("prefecture", "sichyo_sinkyokyoku", "gun_seireishitei", "city", "city_code", "geometry")) %>%
+  mutate(city = if_else(!is.na(gun_seireishitei),
+                        paste(gun_seireishitei, city),
+                        as.character(city)),
+         pref_code = substr(city_code, 1, 2),
+         city_code = as.character(city_code)) %>%
+  select(pref_code, prefecture, sichyo_sinkyokyoku, city_code, city, geometry) %>%
+  st_simplify(preserveTopology = TRUE, dTolerance = 0.0005) %>%
+  st_transform(crs = 4326)
+expect_gte(pryr::object_size(sf_japan), 57.5) # MB
 
-# 各都道府県のshapeからrdsを作成 -----------------------------------------------------
-sf_pref_shp <- function(pref_num) {
-  res <- read_sf(paste0("~/Dropbox/maps/shapefile/国土数値情報/N03/N03-20150101_", sprintf("%02d", pref_num),
-                 "_GML/N03-15_", sprintf("%02d", pref_num), "_150101.shp"),
-          stringsAsFactors = FALSE)
+# 市区町村で一つのPOLYGON、すなわちMULTIPOLYGONにする
+sf_japan_distinct <- rbind(
+  sf_japan %>%
+    filter(city_code != "46527") %>%
+    split(.$pref_code) %>%
+    map(~ .x %>% distinct(city_code, .keep_all = TRUE)) %>%
+    reduce(rbind),
+  sf_japan %>%
+    filter(city_code == "46527") %>%
+    split(.$pref_code) %>%
+    map(~ .x %>% distinct(city_code, .keep_all = TRUE)) %>%
+    reduce(rbind)
+)
 
-  return(res)
+expect_lte(pryr::object_size(sf_japan_distinct), 4831968) # MB
+
+city_union <- function(df) {
+  df %>%
+    split(.$city_code) %>%
+    map(~ st_buffer(st_union(st_buffer(., 0)), dist = 0.0001)) %>%
+    reduce(c) %>%
+    st_sfc()
 }
 
-# sf_pref_shp(pref_num = 1)
-# sf_pref_shp(pref_num = 13)
+tmp_a <- sf_japan %>%
+  filter(city_code == "46527") %>%
+  group_by(city_code) %>%
+  do(out = st_buffer(st_union(st_buffer(., 0)), dist = 0.0001)) %>%
+  use_series(out) %>%
+  purrr::reduce(c) %>%
+  st_sfc()
 
-pref_modified <- function(...) {
+tmp_b <- sf_japan %>%
+  filter(city_code == "46527") %>%
+  city_union()
+expect_true(identical(tmp_a, tmp_b))
+
+# ちょい時間かかる (~15min.)
+sf_japan_distinct$geometry <- c(sf_japan %>%
+                                  filter(city_code != "46527") %>%
+                                  city_union(),
+                                sf_japan %>%
+                                  filter(city_code == "46527") %>%
+                                  city_union())
+
+# 元に戻す
+# 連鎖で実行しない (Errorになる)
+sf_japan_distinct %<>%
+  arrange(city_code)
+
+sf_japan_distinct %>%
+  filter(city == "倉敷市") %>%
+  plot()
+sf_japan %>%
+  filter(city == "倉敷市") %>%
+  plot()
+sf_japan_distinct %>%
+  filter(city == "岡山市 北区") %>%
+  plot()
+sf_japan_distinct %>%
+  filter(grepl("岡山市", city)) %>%
+  plot()
+
+expect_equal(n_distinct(sf_japan_distinct$pref_code), 47L)
+
+pref_modified <- function(prefcode) {
   # st_simplify でwarningが出るのでsuppress
+  pref <- rlang::enquo(prefcode)
+
   res <- suppressWarnings(
-    sf_pref_shp(pref_num = ...) %>%
-    mutate(tmp_var = if_else(is.na(N03_003), "", N03_003),
-           city_name_full = stringi::stri_trim_both(gsub("NA", "", paste(tmp_var, N03_004)))) %>%
-    rename(pref_name = N03_001,
-           # sicho_shinkou_name = N03_002,
-           city_name_ = N03_003, city_name = N03_004, city_code = N03_007) %>%
-    select(pref_name,
-           # sicho_shinkou_name,
-           city_name_, city_name, city_name_full, city_code) %>%
-    mutate_at(.vars = vars(contains("name")), stringi::stri_conv, to = "UTF8") %>%
-    st_simplify(preserveTopology = FALSE, dTolerance = 0.001) %>%
-    filter(!is.na(st_dimension(.)))
-    )
+    sf_japan_distinct %>%
+      filter(pref_code == !!pref) %>%
+      mutate_at(vars(c("prefecture", "sichyo_sinkyokyoku", "city")), stringi::stri_conv, to = "UTF8") %>%
+      st_simplify(preserveTopology = TRUE, dTolerance = 0.0015) %>%
+      filter(!is.na(st_dimension(.)))
+  )
   return(res)
 }
 
-# pref_modified(pref_num = 13) %>%
-#   select(city_name) %>%
-#   plot()
 
-1:47 %>% purrr::map(
-  ~ pref_modified(pref_num = .x) %>% readr::write_rds(path = paste0("inst/extdata/pref_", sprintf("%02s", .x), "_city_spdf.rds"),
-                                                      compress = "xz")
+expect_equal(nrow(pref_modified(prefcode = 33)), 30L)
+expect_equal(nrow(pref_modified(prefcode = 13)), 62L)
+
+dir.create("inst/extdata/ksj_n03/")
+
+# 5MB以内に収める
+1:47 %>% purrr::walk(
+  ~ sprintf("%02d", .x) %>% pref_modified(prefcode = .) %>%
+    readr::write_rds(path = paste0("inst/extdata/ksj_n03/pref_", sprintf("%02s", .x), ".rds"),
+                     compress = "xz")
 )
