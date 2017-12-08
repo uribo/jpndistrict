b410428746d1e5b10b5f4b43b855dd27319fda9c
Shinya Uryu <suika1127@gmail.com>
2017-12-08 14:53:10 +0900

    typo

--- data-raw/ksj_credit_message.R
+++ data-raw/ksj_credit_message.R
@@ -1,4 +1,4 @@
-creditmsg <- "This package provide map data is based on the Digital Map 25000(Map Image)
- published by Geospatial Information Authorityof Japan (Approval No.603FY2017 information usage <http://www.gsi.go.jp>)"
+creditmsg <- "This package provide map data is based on the Digital Map 25000 (Map Image)
+ published by Geospatial Information Authority of Japan (Approval No.603FY2017 information usage <http://www.gsi.go.jp>)"
 
 devtools::use_data(creditmsg, internal = TRUE, overwrite = TRUE)
