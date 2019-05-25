6a2ff6e961a35d52a320133d81f0ed5841ed63fd
Shinya Uryu <suika1127@gmail.com>
2019-05-25 19:40:46 +0900

    Try lintr

--- tic.R
+++ tic.R
@@ -1,5 +1,14 @@
 do_package_checks(error_on = "error")
 
-if (ci_on_travis()) {
-  do_pkgdown()
+get_stage("install") %>%
+  add_code_step(install.packages("lwgeom", configure.args = "--without-liblwgeom"))
+
+if (Sys.getenv("id_rsa") != "") {
+
+  get_stage("before_deploy") %>%
+    add_step(step_setup_ssh())
+
+  get_stage("deploy") %>%
+    add_step(step_build_pkgdown(document = FALSE)) %>%
+    add_step(step_push_deploy(path = "docs", branch = "gh-pages"))
 }
